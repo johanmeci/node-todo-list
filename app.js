a8ae71bf39468dbc66c72f7563d79babb2eb1944
Johan Meneses <johanmeci03@gmail.com>
2022-02-28 17:16:44 -0500

    Adding create and list tasks

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 require('colors');
 
-const { inquirerMenu, pauseMenu } = require('./helpers/inquirer');
-
-console.clear();
+const { inquirerMenu, pauseMenu, getInput } = require('./helpers/inquirer');
+const Tasks = require('./models/tasks');
 
 const main = async() => {
 
     let opt = '';
+    const tasks = new Tasks();
 
     do {
-
+        // Show menu
         opt = await inquirerMenu();
-        console.log({ opt });
+        
+        switch (opt) {
+            case '1':
+
+                const desc = await getInput('Description:');
+                tasks.createTask( desc );
+            break;
+
+            case '2':
+                console.log(tasks.listArr);
+            break;
+        }
 
         await pauseMenu();
         
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -9,31 +9,31 @@ const questions = [
     choices: [
       {
         value: '1',
-        name: '1. Create task'
+        name: `${'1.'.green.bold} Create task`
       },
       {
         value: '2',
-        name: '2. List tasks'
+        name: `${'2.'.green.bold} List tasks`
       },
       {
         value: '3',
-        name: '3. List completed'
+        name: `${'3.'.green.bold} List completed`
       },
       {
         value: '4',
-        name: '4. List pending tasks'
+        name: `${'4.'.green.bold} List pending tasks`
       },
       {
         value: '5',
-        name: '5. Complete tasks'
+        name: `${'5.'.green.bold} Complete tasks`
       },
       {
         value: '6',
-        name: '6. Delete tasks'
+        name: `${'6.'.green.bold} Delete tasks`
       },
       {
         value: '0',
-        name: '0. Exit'
+        name: `${'0.'.green.bold} Exit`
       }
     ]
   }
@@ -43,7 +43,7 @@ const inquirerMenu = async() => {
 
   console.clear();
   console.log('======================'.rainbow);
-  console.log('   Select an option  '.red.bold);
+  console.log('   Select an option  '.bold);
   console.log('======================'.rainbow);
 
   const { option } = await inquirer.prompt(questions);
@@ -67,7 +67,28 @@ const pauseMenu = async() => {
 
 }
 
+const getInput = async( message ) => {
+
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      validate( value ) {
+        if (value.length === 0) {
+          return 'Empty value';
+        }
+        return true;
+      }
+    }
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+}
+
 module.exports = {
   inquirerMenu,
-  pauseMenu
+  pauseMenu,
+  getInput
 }
